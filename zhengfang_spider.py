3140136f11878cb988f96452c9c8baa9871ad024
gufei233 <82166771+gufei233@users.noreply.github.com>
2024-01-22 22:14:36 +0800

    Update zhengfang_spider.py

--- zhengfang_spider.py
+++ zhengfang_spider.py
@@ -118,7 +118,7 @@ class ZhengFangSpider:
             "DNT": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Referer": "http://xk.jwc.zjsru.edu.cn/xs_main.aspx?xh=202206314123",
+            "Referer": f"{self.base_url}/xs_main.aspx?xh={self.student_id}",
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
         }
